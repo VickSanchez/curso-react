6d061f87bda13931e791d8e2e8cee4ee4f9ab15b
VickSanchez <vick_sanchez@outlook.com>
2022-08-23 17:33:06 -0500

    Pruebas en componentes

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -1,10 +1,12 @@
+import { render } from "@testing-library/react"
+import { FirstApp } from "../src/FirstApp"
 
 
 describe('Pruebas en FirstApp', () => { 
 
     test('debe de hacer match con snapshot', () => {
     
-        
+        render( <FirstApp/>)
     
     })
     
