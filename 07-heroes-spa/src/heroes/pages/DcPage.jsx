d8a39108e2b7b521174e13ff6c3544251b2c4632
VickSanchez <vick_sanchez@outlook.com>
2022-08-31 17:58:54 -0500

    Inicio Heroes APP

---
+++ 07-heroes-spa/src/heroes/pages/DcPage.jsx
@@ -0,0 +1,16 @@
+
+export const DcPage = () => {
+    return (
+      <>
+        <h1>DC Comics</h1>
+        <hr />
+        <ul>
+          <li>Superman</li>
+          <li>Batman</li>
+
+        </ul>
+      
+      </>
+    )
+  }
+  
